e34565efdf7b2f47ede30831b343e77e8cea3b39
ramana <ramana.gan@gmail.com>
2020-01-31 14:27:20 -0600

    final commit india development team

---
+++ src/test/java/qaclickacademy/Dmo.java
@@ -0,0 +1,16 @@
+package qaclickacademy;
+
+import org.junit.Test;
+
+public class Dmo {
+	
+	@Test
+	public void URLLogindemo()
+	{
+		System.out.println("This test case for URL logindemo");
+		System.out.println("This test case for URL login from Git Hubdemo");
+		System.out.println("This test case for URL login from Git Hubdemo");
+		
+	}
+
+}
